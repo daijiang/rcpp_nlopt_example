4294a6e7e7b542f713373f67352d9caa0dc0b616
Daijiang Li <dli1@ufl.edu>
2018-01-24 11:36:04 -0500

    on osx

--- main.R
+++ main.R
@@ -1,10 +1,10 @@
 path <- function(x) normalizePath(file.path(getwd(), x))
-dyn.load(path("nlopt/win/x64/lib/libnlopt-0.dll"))
+# dyn.load(path("nlopt/win/x64/lib/libnlopt-0.dll"))
 withr::with_envvar(
   c(
-    PKG_CXXFLAGS = glue::glue('-I"{folder}"', folder = path("nlopt/win/x64/include")),
+    PKG_CXXFLAGS = glue::glue('-I"{folder}"', folder = path("nlopt/osx/include")),
     CXX_STD = "CXX11",
-    PKG_LIBS = glue::glue('-L"{folder}" -lnlopt-0', folder = path("nlopt/win/x64/lib"))
+    PKG_LIBS = glue::glue('-L"{folder}" -lnlopt_cxx', folder = path("nlopt/osx/lib"))
   ),
   Rcpp::sourceCpp('main.cpp', verbose = FALSE, rebuild = TRUE)
 )
